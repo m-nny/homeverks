c308d1d7d8a51bbd09825fcc815ff294b3d88e2f
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-02-07 00:34:28 +0600

    checkpoint

--- HW2/hw2.c
+++ HW2/hw2.c
@@ -7,17 +7,83 @@
 #include <errno.h>
 
 #define BUFLEN 1024
+#define BUFTYPE unsigned char
+#define _asssert(x) if (x == -1) exit(1)
+#define bool _Bool
+#define true 1
+#define false 0
 
+#define IHDR 1229472850
+#define IDAT 1229209940
+#define IEND 1229278788
+
+void * read_to_buffer(int fd, int len) {
+	BUFTYPE * buff = calloc(BUFLEN, sizeof(BUFTYPE));
+	int n = read(fd, buff, len);
+	if (n == -1) {
+		fprintf(stderr, "Probem reading file");
+		exit(1);
+	}
+	if (n != len) {
+		fprintf(stderr, "Couldn't read requested number of bytes (%d, %d)\n", len, n);
+		exit(1);
+	}
+	// for (int i = 0; i < n; i++)
+	// 	buff[i] = htons(buff[i]);
+	return buff;
+}
+
+void print_buff(BUFTYPE * buff, int len) {
+	for (int i = 0; i < len; i++)
+		printf("%02X ", buff[i]);
+	puts("");
+}
+
+void print_str(BUFTYPE * buff, int len) {
+	for (int i = 0; i < len; i++)
+		putchar(buff[i]);
+	puts("");
+}
+
+long buff_to_int(BUFTYPE * buff) {
+	long * np = (long *) buff;
+	return htonl(*np);
+}
+
+bool deal_with_chunk(int fd) {
+	BUFTYPE * buf = read_to_buffer(fd, 8);
+	print_buff(buf, 8);
+	print_str(buf + 4, 4);
+	int len = buff_to_int(buf);
+	int cd = buff_to_int(buf + 4);
+	printf("%d %d\n", len, cd);
+	if (cd == IEND)
+		return 0;
+	if (cd != IDAT) {
+		int offset = lseek(fd, len + 4, SEEK_CUR);
+		_asssert(offset);
+		return 1;
+	}
+	int offset = lseek(fd, len + 4, SEEK_CUR);
+	_asssert(offset);
+	return 1;
+}
 
 int main(int argc, char const *argv[]) {
 	if (argc != 2) {
 		printf("Usage: %s image_name \n", argv[0]);
 		return 1;
 	}
-    int image = open(argv[1], O_RDWR);
-    if (image == -1) {
+  int image = open(argv[1], O_RDWR);
+  if (image == -1) {
 		printf("There was error opening file %s", argv[1]);
 		return 1;
+	}
+	int offset = lseek(image, 8, SEEK_CUR);
+	_asssert(offset);
+
+	while (deal_with_chunk(image)) {
+
 	}
 
 	close(image);
