052d59d5157f60b8ef23c4af5ac2e5cb1b99c4d8
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-02-26 18:48:54 +0600

    checkpoint
    imported as CLion project

--- HW3/simple_shell.c
+++ HW3/simple_shell.c
@@ -6,13 +6,10 @@
 **  Alibek.manabayev@gmail.com
 **  Copyright 2018. All rights reserved.
 */
-#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 #define CMDLEN 80
@@ -23,223 +20,350 @@
 #define true 1
 #define false 0
 
-int parseArguments(char *buff, char **args);
+struct command_t {
+    char *args[ARGCNT];
+    int argc;
+    int in_fd;
+    int out_fd;
+};
+
+typedef struct command_t command;
+
+command *create_command();
+
+void parseArguments(char *buff, command *cmd);
+
 void d_log(char *str);
+
 void d_log2(char *str);
-void child_proc_background(char *arguments[ARGCNT]);
-void child_proc(char *arguments[ARGCNT]);
-bool checkRedOut_a(char **args, int n);
-bool checkRedOut(char **args, int n);
-bool checkRedIn(char **args, int n);
+
+void child_proc_background(command *cmd);
+
+void child_proc(command *cmd);
+
+bool checkRedOut_a(command *cmd);
+
+bool checkRedOut(command *cmd);
+
+bool checkRedIn(command *cmd);
+
+bool checkBackground(command *cmd);
+
+void normalize_cmd(command *cmd);
+
+void free_cmd(command *cmd);
+
+bool run_pipe(command *cmd);
 
 int b_fd = -1;
-int in_fd = 0;
-int out_fd = 1;
-int err_fd = 2;
 
 int main(int argc, char **argv) {
-  if (argc > 1) {
-    b_fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if (b_fd < 0) {
-      puts("Error opening backup file");
-      exit(1);
-    }
-    printf("Backing up to file: %s\n", argv[1]);
-  }
-
-  int i, fd;
-  char buff[CMDLEN];
-  char command[CMDLEN];
-  char *arguments[ARGCNT];
-  bool background = false;
-
-  d_log("Program begins.\n");
-
-  for (;;) {
-    d_log("Please enter a command:   ");
-    fgets(buff, CMDLEN, stdin);
-    d_log2(buff);
-    buff[strlen(buff) - 1] = '\0';
-
-    // printf("|%s|%d\n", buff, strcmp(buff, "quit"));
-
-    if (strcmp(buff, "quit") == 0)
-      break;
-
-    int n = parseArguments(buff, arguments);
-
-    background = n > 0 && !strcmp(arguments[n - 1], "&");
-
-    if (background) {
-      arguments[n - 1] = NULL;
-      n--;
+    if (argc > 1) {
+        b_fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+        if (b_fd < 0) {
+            puts("Error opening backup file");
+            exit(1);
+        }
+        printf("Backing up to file: %s\n", argv[1]);
     }
 
-    in_fd = dup(0);
-    out_fd = dup(1);
-    err_fd = dup(2);
+    char buff[CMDLEN];
+    bool background = false;
+    command *cur_cmd;
 
-    for (int x = 0; x < 3; x++) {
-      if (checkRedIn(arguments, n))
-        n -= 2;
-      if (checkRedOut(arguments, n))
-        n -= 2;
-      if (checkRedOut_a(arguments, n))
-        n -= 2;
+    d_log("Program begins.\n");
+
+    for (;;) {
+        d_log("Please enter a command:   ");
+        fgets(buff, CMDLEN, stdin);
+        d_log2(buff);
+        buff[strlen(buff) - 1] = '\0';
+
+        // printf("|%s|%d\n", buff, strcmp(buff, "quit"));
+
+        if (strcmp(buff, "quit") == 0)
+            break;
+
+        cur_cmd = create_command();
+        parseArguments(buff, cur_cmd);
+
+        background = checkBackground(cur_cmd);
+
+        for (int x = 0; x < 3; x++) {
+            checkRedIn(cur_cmd);
+            checkRedOut(cur_cmd);
+            checkRedOut_a(cur_cmd);
+        }
+        normalize_cmd(cur_cmd);
+        // printf("|%d|%d|%d|\n", in_fd, out_fd, err_fd);
+        if (run_pipe(cur_cmd)) {
+
+        } else {
+            if (background) {
+                child_proc_background(cur_cmd);
+            } else {
+                child_proc(cur_cmd);
+            }
+        }
+        free_cmd(cur_cmd);
     }
-    // printf("|%d|%d|%d|\n", in_fd, out_fd, err_fd);
-    if (background) {
-      child_proc_background(arguments);
-    } else {
-      child_proc(arguments);
+}
+
+command *create_command() {
+    command *p = malloc(sizeof(command));
+    p->argc = 0;
+    p->in_fd = -1;
+    p->out_fd = -1;
+    return p;
+}
+
+void normalize_cmd(command *cmd) {
+    if (cmd->in_fd == -1) {
+        cmd->in_fd = dup(0);
     }
-    close(in_fd);
-    close(out_fd);
-    close(err_fd);
-  }
+    if (cmd->out_fd == -1) {
+        cmd->out_fd = dup(1);
+    }
 }
 
-int parseArguments(char *buff, char **args) {
-  int i = 0;
-  args[i] = strtok(buff, SEPARATOR);
-  while (args[i] != NULL) {
-    args[++i] = strtok(NULL, SEPARATOR);
-  }
-  return i;
+void free_cmd(command *cmd) {
+    close(cmd->in_fd);
+    close(cmd->out_fd);
+    free(cmd);
 }
 
-bool checkRedIn(char **args, int n) {
-  if (n < 3) {
-    return 0;
-  }
-  if (strcmp(args[n - 2], "<") != 0) {
-    return 0;
-  }
-  in_fd = open(args[n - 1], O_RDONLY);
-  if (in_fd < 0) {
-    d_log("Error in opening file.\n");
-    exit(1);
-  }
-  args[n - 1] = NULL;
-  args[n - 2] = NULL;
-  return 1;
+void parseArguments(char *buff, command *cmd) {
+    int i = 0;
+    cmd->args[i] = strtok(buff, SEPARATOR);
+    while (cmd->args[i] != NULL) {
+        cmd->args[++i] = strtok(NULL, SEPARATOR);
+    }
+    cmd->argc = i;
 }
 
-bool checkRedOut(char **args, int n) {
-  if (n < 3) {
-    return 0;
-  }
-  if (strcmp(args[n - 2], ">") != 0) {
-    return 0;
-  }
-  out_fd = open(args[n - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (out_fd < 0) {
-    d_log("Error in opening file.\n");
-    exit(1);
-  }
-  args[n - 1] = NULL;
-  args[n - 2] = NULL;
-  return 1;
+bool checkRedIn(command *cmd) {
+    int n = cmd->argc;
+    if (n < 3) {
+        return 0;
+    }
+    if (strcmp(cmd->args[n - 2], "<") != 0) {
+        return 0;
+    }
+    cmd->in_fd = open(cmd->args[n - 1], O_RDONLY);
+    if (cmd->in_fd < 0) {
+        d_log("Error in opening file.\n");
+        exit(1);
+    }
+    cmd->args[n - 1] = NULL;
+    cmd->args[n - 2] = NULL;
+    cmd->argc -= 2;
+    return 1;
 }
 
-bool checkRedOut_a(char **args, int n) {
-  if (n < 3) {
-    return 0;
-  }
-  if (strcmp(args[n - 2], ">>") != 0) {
-    return 0;
-  }
-  out_fd = open(args[n - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
-  if (in_fd < 0) {
-    d_log("Error in opening file.\n");
-    exit(1);
-  }
-  args[n - 1] = NULL;
-  args[n - 2] = NULL;
-  return 1;
+bool checkRedOut(command *cmd) {
+    int n = cmd->argc;
+    if (n < 3) {
+        return 0;
+    }
+    if (strcmp(cmd->args[n - 2], ">") != 0) {
+        return 0;
+    }
+    cmd->out_fd = open(cmd->args[n - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (cmd->out_fd < 0) {
+        d_log("Error in opening file.\n");
+        exit(1);
+    }
+    cmd->args[n - 1] = NULL;
+    cmd->args[n - 2] = NULL;
+    cmd->argc -= 2;
+    return 1;
+}
+
+bool checkRedOut_a(command *cmd) {
+    int n = cmd->argc;
+    if (n < 3) {
+        return 0;
+    }
+    if (strcmp(cmd->args[n - 2], ">>") != 0) {
+        return 0;
+    }
+    cmd->out_fd = open(cmd->args[n - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (cmd->out_fd < 0) {
+        d_log("Error in opening file.\n");
+        exit(1);
+    }
+    cmd->args[n - 1] = NULL;
+    cmd->args[n - 2] = NULL;
+    cmd->argc -= 2;
+    return 1;
+}
+
+bool checkBackground(command *cmd) {
+    int n = cmd->argc;
+    if (n < 2) {
+        return false;
+    }
+    if (strcmp(cmd->args[n - 1], "&") != 0) {
+        return false;
+    }
+    cmd->args[n - 1] = NULL;
+    (cmd->argc)--;
+    return true;
 }
 
 void tee(int in, int out1, int out2) {
-  char BUFF[BUFFLEN];
-  int n = 0, offset = 0;
-  // fputs("-- take a cop of tee --", stderr);
-  while ((n = read(in, BUFF, BUFFLEN)) > 0) {
-    // fprintf(stderr, "|>%s(%d)<|\n", BUFF, n);
-    if (out1 >= 0) {
-      offset = write(out1, BUFF, n);
-      if (offset < 0) {
-        d_log("error teeing to out1\n");
-        exit(1);
-      }
+    char BUFF[BUFFLEN];
+    int n = 0, offset = 0;
+    // fputs("-- take a cop of tee --", stderr);
+    while ((n = (int) read(in, BUFF, BUFFLEN)) > 0) {
+        // fprintf(stderr, "|>%s(%d)<|\n", BUFF, n);
+        if (out1 >= 0) {
+            offset = (int) write(out1, BUFF, (size_t) n);
+            if (offset < 0) {
+                d_log("error teeing to out1\n");
+                exit(1);
+            }
+        }
+        if (out2 >= 0) {
+            offset = (int) write(out2, BUFF, (size_t) n);
+            if (offset < 0) {
+                d_log("error teeing to out2\n");
+                exit(1);
+            }
+        }
     }
-    if (out2 >= 0) {
-      offset = write(out2, BUFF, n);
-      if (offset < 0) {
-        d_log("error teeing to out2\n");
-        exit(1);
-      }
-    }
-  }
-  // fputs("-- enough --", stderr);
+    // fputs("-- enough --", stderr);
 }
 
-void child_proc(char *arguments[ARGCNT]) {
-  int childpid;
-  int status;
-  int fds[2];
-
-  pipe(fds);
-
-  childpid = fork();
-
-  if (childpid < 0) {
-    d_log("Error in fork.\n");
-    exit(-1);
-  }
-
-  if (childpid != 0) {
-    /* Parent process closes up output side of pipe */
-    close(fds[1]);
-
-    /* Read in a string from the pipe (fds[0])*/
-    tee(fds[0], out_fd, b_fd);
-    waitpid(childpid, &status, 0);
-    close(fds[0]);
-  } else {
-    /* Child process closes up input side of pipe */
-    close(fds[0]);
-		dup2(in_fd, 0);
-    dup2(fds[1], 1);
-    execvp(arguments[0], arguments);
-
-    exit(0);
-  }
+command *split_commands(command *cmd) {
+    int x = -1;
+    for (int i = 0; i < cmd->argc; i++) {
+        if (strcmp(cmd->args[i], "|") == 0) {
+            x = i;
+            break;
+        }
+    }
+    fprintf(stderr, "|%s(%d)|>%d<\n", cmd->args[0], cmd->argc, x);
+    if (x == -1) {
+        return NULL;
+    }
+    command *rm = create_command();
+    int y = 0;
+    for (int i = x + 1; i < cmd->argc; i++) {
+        rm->args[y++] = cmd->args[i];
+        cmd->args[i] = NULL;
+    }
+    rm->argc = y;
+    cmd->argc = x;
+    rm->args[y] = NULL;
+    cmd->args[x] = NULL;
+    for (int i = 0; i < cmd->argc; i++) {
+        fprintf(stderr, "|%s", cmd->args[i]);
+    }
+    fprintf(stderr, "|\n");
+    for (int i = 0; i < rm->argc; i++) {
+        fprintf(stderr, "|%s", rm->args[i]);
+    }
+    fprintf(stderr, "|\n");
+    fprintf(stderr, "\\\\_____//\n");
+    return rm;
 }
 
-void child_proc_background(char *arguments[ARGCNT]) {
-  int childpid;
-  int status;
+bool run_pipe(command *cmd) {
+    int total_in = cmd->in_fd;
+    int total_out = cmd->out_fd;
 
-  childpid = fork();
+    command *rest = split_commands(cmd);
+    if (rest == NULL) {
+        return 0;
+    }
+    int childpid;
+    int status;
+    int fds[2];
 
-  if (childpid < 0) {
-    d_log("Error in fork.\n");
-    exit(-1);
-  }
+    pipe(fds);
+    childpid = fork();
+    if (childpid < 0) {
+        d_log("Error in fork.\n");
+        exit(1);
+    }
+    if (childpid == 0) {
+        /* Child process closes up input side of pipe */
+        close(fds[0]);
+        cmd->in_fd = total_in;
+        cmd->out_fd = fds[1];
+        child_proc(cmd);
+        exit(0);
+    } else {
+        /* Parent process closes up output side of pipe */
+        close(fds[1]);
 
-  if (childpid != 0) {
-    return;
-  } else {
-    child_proc(arguments);
-    exit(0);
-  }
+        /* Read in a string from the pipe (fds[0])*/
+        rest->in_fd = fds[0];
+        rest->out_fd = total_out;
+        run_pipe(rest);
+        waitpid(childpid, &status, 0);
+        close(fds[0]);
+    }
+    return 1;
+}
+
+void child_proc(command *cmd) {
+    int childpid;
+    int status;
+    int fds[2];
+
+    pipe(fds);
+
+    childpid = fork();
+
+    if (childpid < 0) {
+        d_log("Error in fork.\n");
+        exit(1);
+    }
+
+    if (childpid != 0) {
+        /* Parent process closes up output side of pipe */
+        close(fds[1]);
+
+        /* Read in a string from the pipe (fds[0])*/
+        tee(fds[0], cmd->out_fd, b_fd);
+        waitpid(childpid, &status, 0);
+        close(fds[0]);
+    } else {
+        /* Child process closes up input side of pipe */
+        close(fds[0]);
+        dup2(cmd->in_fd, 0);
+        dup2(fds[1], 1);
+        execvp(cmd->args[0], cmd->args);
+
+        exit(0);
+    }
+}
+
+void child_proc_background(command *cmd) {
+    int childpid;
+
+    childpid = fork();
+
+    if (childpid < 0) {
+        d_log("Error in fork.\n");
+        exit(1);
+    }
+
+    if (childpid != 0) {
+        return;
+    } else {
+        child_proc(cmd);
+        exit(0);
+    }
 }
 
 void d_log(char *str) {
-  printf("%s", str);
-  if (b_fd != -1) {
-    d_log2(str);
-  }
+    printf("%s", str);
+    if (b_fd != -1) {
+        d_log2(str);
+    }
 }
 
 void d_log2(char *str) { write(b_fd, str, strlen(str)); }
