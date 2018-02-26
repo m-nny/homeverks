dc76b331e0f4d881e86c61789e4c6790abfecd12
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-02-26 18:46:17 +0600

    part3
    implemented 3 level forking

--- HW3/simple_shell.c
+++ HW3/simple_shell.c
@@ -3,20 +3,77 @@
 **	single word commands (no arguments).
 **	Type "quit" to quit.
 */
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #define CMDLEN 80
 #define ARGCNT 10
+#define BUFFLEN 2048
 #define SEPARATOR " "
 #define bool _Bool
 #define true 1
 #define false 0
+
+int parseArguments(char *buff, char **args);
+void tee(int in, int out1, int out2);
+void d_log(char * str);
+void child_proc_background(char * arguments[ARGCNT]);
+void child_proc(char * arguments[ARGCNT]);
+int b_fd = -1;
+
+int main(int argc, char ** argv) {
+	if (argc > 1) {
+		b_fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+		if (b_fd < 0) {
+			puts("Error opening backup file");
+			exit(1);
+		}
+		printf("Backing up to file: %s\n", argv[1]);
+	}
+
+  int   i, fd;
+  char  buff[CMDLEN];
+  char  command[CMDLEN];
+  char *arguments[ARGCNT];
+  bool  background = false;
+
+  d_log("Program begins.\n");
+
+  for (;;) {
+    d_log("Please enter a command:   ");
+    fgets(buff, CMDLEN, stdin);
+		d_log(buff);
+    buff[strlen(buff) - 1] = '\0';
+
+    // printf("|%s|%d\n", buff, strcmp(buff, "quit"));
+
+    if (strcmp(buff, "quit") == 0) break;
+
+    int n = parseArguments(buff, arguments);
+
+    // for (int i = 0; arguments[i] != NULL; i++) {
+    //   printf("\\%s/\n", arguments[i]);
+    // }
+
+    background = n > 0 && !strcmp(arguments[n - 1], "&");
+
+    if (background) {
+      arguments[n - 1] = NULL;
+    }
+		if (background) {
+			child_proc_background(arguments);
+		} else {
+			child_proc(arguments);
+		}
+  }
+}
 
 int parseArguments(char *buff, char **args) {
   int i = 0;
@@ -32,52 +89,68 @@ int parseArguments(char *buff, char **args) {
   return i;
 }
 
-int main() {
-  int   pid;
+void tee(int in, int out1, int out2) {
+	char BUFF[BUFFLEN];
+	int n = 0;
+	// puts("-- take a cop of tee --");
+	while ((n = read(in, BUFF, BUFFLEN)) > 0) {
+		// printf("|%s(%d)|", BUFF, n);
+		write(out1, BUFF, n);
+		write(out2, BUFF, n);
+	}
+	// puts("-- enough --");
+}
+
+void child_proc(char *arguments[ARGCNT]) {
+  int   childpid;
   int   status;
-  int   i, fd;
-  char  buff[CMDLEN];
-  char  command[CMDLEN];
-  char *arguments[ARGCNT];
-  bool  background = false;
+	int fds[2];
 
-  printf("Program begins.\n");
+	pipe(fds);
 
-  for (;;) {
-    printf("Please enter a command:   ");
-    fgets(buff, CMDLEN, stdin);
-    buff[strlen(buff) - 1] = '\0';
+	childpid = fork();
 
-    // printf("|%s|%d\n", buff, strcmp(buff, "quit"));
+	if (childpid < 0) {
+	  d_log("Error in fork.\n");
+	  exit(-1);
+	}
 
-    if (strcmp(buff, "quit") == 0) break;
+	if (childpid != 0) {
+	  /* Parent process closes up output side of pipe */
+	  close(fds[1]);
 
-    int n = parseArguments(buff, arguments);
+	  /* Read in a string from the pipe (fds[0])*/
+		tee(fds[0], 1, b_fd);
+    waitpid(childpid, &status, 0);
+	} else {
+	  /* Child process closes up input side of pipe */
+	  close(fds[0]);
+		dup2(fds[1], 1);
+		execvp(arguments[0], arguments);
 
-    for (int i = 0; arguments[i] != NULL; i++) {
-      printf("\\%s/\n", arguments[i]);
-    }
+		exit(0);
+	}
+}
 
-    background = n > 0 && !strcmp(arguments[n - 1], "&");
+void child_proc_background(char * arguments[ARGCNT]) {
+  int   childpid;
+  int   status;
 
-    if (background) {
-      arguments[n - 1] = NULL;
-    }
+	childpid = fork();
 
-    pid = fork();
-
-    if (pid < 0) {
-      printf("Error in fork.\n");
-      exit(-1);
-    }
-
-    if (pid != 0) {
-      if (!background) {
-        waitpid(pid, &status, 0);
-      }
-    } else {
-      execvp(arguments[0], arguments);
-      break;
-    }
+  if (childpid < 0) {
+    d_log("Error in fork.\n");
+    exit(-1);
   }
+
+  if (childpid != 0) {
+
+  } else {
+		child_proc(arguments);
+		exit(0);
+  }
+}
+
+void d_log(char * str) {
+	printf("%s", str);
 }
