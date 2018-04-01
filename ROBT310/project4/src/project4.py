0c055c7b62f68ecda97825e4a6151437544b9855
Alibek Manabayev <Alibek.manabayev@gmail.com>
2018-04-01 19:23:19 +0600

    checkpoint

--- ROBT310/project4/src/project4.py
+++ ROBT310/project4/src/project4.py
@@ -2,78 +2,94 @@ import sys
 import cv2 as cv
 import numpy as np
 
-DELAY_CAPTION = 1500
-DELAY_BLUR = 100
-MIN_KERNEL_LENGTH = 10
-MAX_KERNEL_LENGTH = 255
+image_window = "Frame"
+config_window = "Config"
+cap = None
 
-src = None
-dst = None
-window_name = 'Project 4'
+MouseX, MouseY = (0, 0)
+
+
+def nothing(val):
+    print(val)
+
+
+def setup():
+    print("Setup")
+    global cap
+    cap = cv.VideoCapture(0)
+    cv.namedWindow(image_window, cv.WINDOW_AUTOSIZE)
+    cv.namedWindow(config_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(config_window, 720, 80)
+
+    cv.createTrackbar('R', config_window, 0, 255, nothing)
+    cv.createTrackbar('G', config_window, 0, 255, nothing)
+    cv.createTrackbar('B', config_window, 0, 255, nothing)
+    cv.createTrackbar('Radius', config_window, 0, 255, nothing)
+
+    # cap.set(cv.CAP_PROP_FPS, 1)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+    cv.setMouseCallback(image_window, get_pixel, None)
+    print("Setup ended")
+
+
+def run():
+    print("run")
+    global cap, frame
+    global MouseX, MouseY
+    while True:
+        _, frame = cap.read()
+
+        # get current positions of four trackbars
+        r = cv.getTrackbarPos('R', config_window)
+        g = cv.getTrackbarPos('G', config_window)
+        b = cv.getTrackbarPos('B', config_window)
+        radius = cv.getTrackbarPos('Radius', config_window)
+
+        f_frame = selective_color(frame, (b, g, r), radius)
+
+        cv.imshow(image_window, f_frame)
+
+        k = cv.waitKey(5) & 0xFF
+        if k == 27:
+            break
+
+        # print("{},{}".format((b, g, r), radius))
+        # print("({},{})={}".format(MouseX, MouseY, frame[MouseY][MouseX]))
+    print("Finished running")
 
 
 def main(argv):
-    cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
-
-    # Load the source image
-    image_name = argv[0] if len(argv) > 0 else "../data/lena3.tif"
-    global src
-    src = cv.imread(image_name)
-    if src is None:
-        print('Error opening image')
-        print(
-            'Usage: smoothing.py [image_name -- default ../data/lena3.tif] \n')
-        return -1
-
-    if display_caption('Original Image') != 0:
-        return 0
-
-    global dst
-    dst = np.copy(src)
-    if display_dst(DELAY_CAPTION) != 0:
-        return 0
-    # Selective color effect
-    if display_caption('Selective color effect') != 0:
-        return 0
-
-    for i in range(MIN_KERNEL_LENGTH, MAX_KERNEL_LENGTH, 5):
-        dst = selective_color(src, (0, 0, 255), i)
-        if display_dst(DELAY_BLUR) != 0:
-            return 0
-
-    #  Done
-    display_caption('Done!')
+    global cap
+    setup()
+    run()
+    cap.release()
     return 0
+
+
+def get_pixel(event, x, y, flags, param):
+    global MouseX, MouseY, frame
+    if event == cv.EVENT_LBUTTONDOWN:
+        MouseX, MouseY = x, y
+        b, g, r = frame[y][x]
+        cv.setTrackbarPos('R', config_window, r)
+        cv.setTrackbarPos('G', config_window, g)
+        cv.setTrackbarPos('B', config_window, b)
+        print("Set RGB to {}".format((r, g, b)))
 
 
 def selective_color(image, point, radius):
     # n, m, d = image.shape
-    mask = np.linalg.norm(image - point, axis=2) > radius
-    # mask = (np.sum((image - point) ** 2, axis=2)) > (radius ** 2)
+    # mask = np.linalg.norm(image - point, axis=2) > radius
+    # mask = 0
+    mask = (np.sum((image - point) ** 2, axis=2)) > (radius ** 2)
     # n_mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    gray = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
+    gray = cv.merge((gray, gray, gray))
+    # gray = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
     output = np.copy(image)
     output[mask] = gray[mask]
     return output
-
-
-def display_caption(caption):
-    global dst
-    dst = np.zeros(src.shape, src.dtype)
-    rows, cols, ch = src.shape
-    cv.putText(dst, caption,
-               (int(cols / 4), int(rows / 2)),
-               cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
-    return display_dst(DELAY_CAPTION)
-
-
-def display_dst(delay):
-    cv.imshow(window_name, dst)
-    c = cv.waitKey(delay)
-    if c >= 0:
-        return -1
-    return 0
 
 
 if __name__ == "__main__":
